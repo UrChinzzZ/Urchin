e99325c5cbb43c657ac4b42eb609e7fe4b745e07
user <user@192.168.1.116>
2019-08-15 16:01:39 +0800

    test

--- src/main/java/com/urchin/DesignPatterns/FactoryModel/SimpleFactory/Test.java
+++ src/main/java/com/urchin/DesignPatterns/FactoryModel/SimpleFactory/Test.java
@@ -49,16 +49,16 @@ public class Test {
     }
 	//给sb书豪的代码优化
     public static void  calculate() {
-    	algorithm algorithm=new algorithm_impl();
-        Scanner scanner=new Scanner(System.in);
-        Scanner scanner3=new Scanner(System.in);
+    	algorithm algorithm = new algorithm_impl();
+        Scanner scanner = new Scanner(System.in);
+        Scanner scanner3 = new Scanner(System.in);
         System.out.println("请输入第一个数");
-        int a=scanner.nextInt();
-        Scanner scanner1=new Scanner(System.in);
+        int a = scanner.nextInt();
+        Scanner scanner1 = new Scanner(System.in);
         System.out.println("请输入第二个数");
-        int b=scanner1.nextInt();
+        int b = scanner1.nextInt();
         System.out.println("选择计算方法 1：加法，2:乘法，3：减法，4：除法");
-        int all=scanner3.nextInt();
+        int all = scanner3.nextInt();
         switch(all) {
         	case 1:
                 System.out.println("结果是："+ algorithm.addition(a,b));
@@ -73,6 +73,9 @@ public class Test {
         		 System.out.println("结果是："+algorithm.division(a,b));
         		 break;	
         }    
+        scanner.close();
+        scanner3.close();
+        scanner1.close();
     }
 
 
